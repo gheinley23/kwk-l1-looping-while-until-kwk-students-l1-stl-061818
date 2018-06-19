caaeb0a373bd72c1019f3eb01fdeec5484a5152f
gheinley23 <gheinley23@jacksonindians.us>
2018-06-19 21:11:37 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
 def using_until
         levitation_force = 6
         #your code here
-    
+    loop do 
+      if levitation_force = 10
+        break
+      end
+      puts "levitate higher"
+      sleep(0.7)
+      
 end
 
